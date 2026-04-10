bca40d553e62ea3dac1d498d1ca039d222237999
dz334 <dannyzhang2004@gmail.com>
2026-04-10 11:12:44 -0400

    Update assets.lua

--- Field Tested/assets.lua
+++ Field Tested/assets.lua
@@ -2,6 +2,13 @@ local assets = {}
 
 function assets.load()
     assets.background1 = {
+        backgroundCloud1 = love.graphics.newImage('Map/Background/Cloud_cover_1.png'),
+        backgroundCloud2 = love.graphics.newImage('Map/Background/Cloud_cover_2.png'),
+        backgroundHills = love.graphics.newImage('Map/Background/Hills.png'),
+        backgroundSky = love.graphics.newImage('Map/Background/Sky_color.png')
+    }
+
+    assets.background2 = {
         backgroundCloud1 = love.graphics.newImage('Tiles/Seaside/Background/Clouds_big.png'),
         backgroundCloud2 = love.graphics.newImage('Tiles/Seaside/Background/Clouds_medium.png'),
         backgroundCloud3 = love.graphics.newImage('Tiles/Seaside/Background/Clouds_small.png'),
@@ -15,24 +22,55 @@ function assets.load()
         gameMusic = 'Sounds/AccumulaTown.mp3'
     }
 
-    assets.sprite1 = {
-
-    }
-
     assets.character1 = {
+        idleLeft = love.graphics.newImage('Sprites/Character1/IdleLeft.png'),
+        idleRight = love.graphics.newImage('Sprites/Character1/IdleRight.png'),
 
+        jumpLeft = love.graphics.newImage('Sprites/Character1/JumpLeft.png'),
+        jumpRight = love.graphics.newImage('Sprites/Character1/JumpRight.png'),
+
+        runLeft = love.graphics.newImage('Sprites/Character1/RunLeft.png'),
+        runRight = love.graphics.newImage('Sprites/Character1/RunRight.png'),
+
+        fallLeft = love.graphics.newImage('Sprites/Character1/FallLeft.png'),
+        fallRight = love.graphics.newImage('Sprites/Character1/FallRight.png'),
+        
+        doubleJumpLeft = love.graphics.newImage('Sprites/Character1/Double_Jump_Left.png'),
+        doubleJumpRight = love.graphics.newImage('Sprites/Character1/Double_Jump_Right.png')
     }
 
     assets.character2 = {
+        idleLeft = love.graphics.newImage('Sprites/Character2/IdleLeft.png'),
+        idleRight = love.graphics.newImage('Sprites/Character2/IdleRight.png'),
+
+        jumpLeft = love.graphics.newImage('Sprites/Character2/JumpLeft.png'),
+        jumpRight = love.graphics.newImage('Sprites/Character2/JumpRight.png'),
+
+        runLeft = love.graphics.newImage('Sprites/Character2/RunLeft.png'),
+        runRight = love.graphics.newImage('Sprites/Character2/RunRight.png'),
+
+        fallLeft = love.graphics.newImage('Sprites/Character2/FallLeft.png'),
+        fallRight = love.graphics.newImage('Sprites/Character2/FallRight.png'),
         
+        doubleJumpLeft = love.graphics.newImage('Sprites/Character2/Double_Jump_Left.png'),
+        doubleJumpRight = love.graphics.newImage('Sprites/Character2/Double_Jump_Right.png')
     }
 
     assets.character3 = {
-        idle = love.graphics.newImage('Sprites/Character3/Idle.png'),
-        jump = love.graphics.newImage('Sprites/Character3/Jump.png'),
-        run = love.graphics.newImage('Sprites/Character3/Run.png'),
-        fall = love.graphics.newImage('Sprites/Character3/Fall.png'),
-        doubleJump = love.graphics.newImage('Sprites/Character3/Double_Jump.png')
+        idleLeft = love.graphics.newImage('Sprites/Character3/IdleLeft.png'),
+        idleRight = love.graphics.newImage('Sprites/Character3/IdleRight.png'),
+
+        jumpLeft = love.graphics.newImage('Sprites/Character3/JumpLeft.png'),
+        jumpRight = love.graphics.newImage('Sprites/Character3/JumpRight.png'),
+
+        runLeft = love.graphics.newImage('Sprites/Character3/RunLeft.png'),
+        runRight = love.graphics.newImage('Sprites/Character3/RunRight.png'),
+
+        fallLeft = love.graphics.newImage('Sprites/Character3/FallLeft.png'),
+        fallRight = love.graphics.newImage('Sprites/Character3/FallRight.png'),
+        
+        doubleJumpLeft = love.graphics.newImage('Sprites/Character3/Double_Jump_Left.png'),
+        doubleJumpRight = love.graphics.newImage('Sprites/Character3/Double_Jump_Right.png')
     }
 
     assets.character4 = {
@@ -50,15 +88,6 @@ function assets.load()
         
         doubleJumpLeft = love.graphics.newImage('Sprites/Character4/Double_Jump_Left.png'),
         doubleJumpRight = love.graphics.newImage('Sprites/Character4/Double_Jump_Right.png')
-    }
-
-    assets.ui = {
-        panel = love.graphics.newImage('Tiles/4.png')
-    }
-
-    assets.orb = {
-        orbIdle = love.graphics.newImage('Sprites/Key/key.png')
-        --orbCollected = love.graphics.newImage('Tiles/vfx_effect_orb.png')
     }
 end
 
